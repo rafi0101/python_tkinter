2b85a8f8194ab4ad17771b84d5ff9346335f56f3
lukas1505 <lukas909009@gmail.com>
2020-08-20 22:09:26 +0200

    add test01 programm ( show picture in tkinter )

---
+++ test01.py
@@ -0,0 +1,17 @@
+#this should be a little ui with an picture in it
+# import tkinter as tk
+
+import tkinter as tk
+# importiert tinker als tk
+from tkinter import PhotoImage
+# import photoimage packet von tkinter um fotos darzustellen
+programm = tk.Tk()
+# erstellt ein fenster namens programm
+logo = PhotoImage(file="homer.gif")
+# importiert foto 
+label = tk.Label(programm, image=logo).pack(side="top")
+# weist foto ein paar eigenschaften zu 
+programm.mainloop()
+# main loop schleife muss immer am ende stehen
+# aktualisiert das programm wärend der laufzeit
+
