fd62c9d5786f7adb9e476c158b5e30d6bf9d1e92
lukas1505 <lukas909009@gmail.com>
2020-08-31 18:46:45 +0200

    ADD label.py

---
+++ tkinterlabel/label.py
@@ -0,0 +1,24 @@
+import tkinter
+
+
+#erstellt ein kleines tkinter fenster
+root = tkinter.Tk()
+#gibt dem fenster einen Namen oder eine Überschrift je nachdem
+root.title("Hello World")
+#Gibt an dass die Größe des Fensters nicht verändert werden darf
+root.resizable(0,0)
+# Gibt die Hintergrundfarbe Blau an
+root.config(bg="blue")
+#Gibt die Größe des Fensters an also 400 mal 400 pixel
+# root.geometry("400x400")
+# bringt das tkinter Fenster in den Vollbildmodus
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.geometry("%dx%d+0+0" % (w, h))
+root.mainloop()
+# erstellt ein Label das einen Text im Fenster root angibt mit dem Inhalt Hello world
+label1 = tkinter.Label(root, text="hello world")
+#gibt das label aus auf dem Fenster root
+label1.pack()
+
+#mainloop muss immer am Ende stehen
+root.mainloop()
