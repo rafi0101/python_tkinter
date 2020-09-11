95caecb58cf54ea8dd269cc504535b5c08071975
Lukas Paul <lukas909009@gmail.com>
2020-09-11 22:53:04 +0200

    chg root.py

--- tkinter_start_programms/root.py
+++ tkinter_start_programms/root.py
@@ -11,16 +11,40 @@ positionRight = int(programm.winfo_screenwidth()/3.5 - windowWidth/3.5)
 positionDown = int(programm.winfo_screenheight()/3.5 - windowHeight/3.5)
 
 programm.geometry("+{}+{}".format(positionRight, positionDown))
-programm.title("Games_Starter")
+programm.title("Application_Starter")
 programm.iconphoto
 programm.config(background="#4287f5")
-programm.iconbitmap('C:/Users/lukas_paul/Downloads/battlefront.ico')
-header = tk.Label(programm, font="arial", bg="#4287f5", text="Starten Sie Battlefront wenn sie klicken\n")
+programm.iconbitmap('C:/Users/lukas/python_tkinter/tkinter_start_programms/assets/battlefront.ico')
+header = tk.Label(programm, font="arial", bg="#4287f5", text="\n")
 header.pack()
+def programmieren():
+    # header1 = tk.Label(programm, text="\nBattlefront startet", bg="#4287f5")
+    # header1.pack()
+    # subprocess.call(['C:/Program Files (x86)/Origin Games/STAR WARS Battlefront II/starwarsbattlefrontii.exe'])
+    subprocess.call(['C:/Users/lukas/AppData/Local/Vivaldi/Application/vivaldi.exe'])
+    subprocess.call(['C:/Users/lukas/AppData/Local/Programs/Microsoft VS Code/Code.exe'])
+    # subprocess.call(['%SystemRoot%/system32/WindowsPowerShell/v1.0/powershell.exe'])
 def battlefront():
-    header1 = tk.Label(programm, text="\nBattlefront startet", bg="#4287f5")
-    header1.pack()
-button = tk.Button(programm, text='Start', width=25, command=battlefront)
+    subprocess.call(['C:/Program Files (x86)/Origin Games/STAR WARS Battlefront II/starwarsbattlefrontii.exe'])
+def rainbow():
+    subprocess.call(["C:/Program Files (x86)/Steam/steamapps/common/Tom Clancy's Rainbow Six Siege/RainbowSix.exe"])
+def warzone():
+    subprocess.call(["C:/Program Files (x86)/Call of Duty Modern Warfare/Modern Warfare Launcher.exe"])
+button = tk.Button(programm, text='Programmieren', width=25, command=programmieren)
 button.pack()
+header1 = tk.Label(programm, bg="#4287f5", text="\n")
+header1.pack()
+button1 = tk.Button(programm, text='Battlefront', width=25, command=battlefront)
+button1.pack()
+header2 = tk.Label(programm, bg="#4287f5", text="\n")
+header2.pack()
+button2 = tk.Button(programm, text='Rainbow', width="25", command=rainbow)
+button2.pack()
+header3 = tk.Label(programm, bg="#4287f5", text="\n")
+header3.pack()
+button3 = tk.Button(programm, text='warzone', width="25", command=warzone)
+button3.pack()
+
+
 
 programm.mainloop()
